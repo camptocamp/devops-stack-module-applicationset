camptocamp/devops-stack-module-applicationset

commit 80cff79d8a7d0e2cf5d0fc548c5e9732848c1c2b

feat!: add support to oboukili/argocd v5 (#26)
* feat!: add support to oboukili/argocd v5

* docs(terraform-docs): generate docs and write to README.adoc

---------

Co-authored-by: hbollon <hbollon@users.noreply.github.com>

main.tf
@@ -3,7 +3,7 @@ resource "null_resource" "dependencies" {
 }
 
 resource "argocd_repository" "private_https_repo" {
-  # This count here is nothing more than a way to conditionally deploy this resource. Although there is no loop inside 
+  # This count here is nothing more than a way to conditionally deploy this resource. Although there is no loop inside
   # the resource, if the condition is true, the resource is deployed because there is exactly one iteration.
   count = (var.source_credentials_https.password != null && startswith(var.project_source_repo, "https://")) ? 1 : 0
 
@@ -14,7 +14,7 @@ resource "argocd_repository" "private_https_repo" {
 }
 
 resource "argocd_repository" "private_ssh_repo" {
-  # This count here is nothing more than a way to conditionally deploy this resource. Although there is no loop inside 
+  # This count here is nothing more than a way to conditionally deploy this resource. Although there is no loop inside
   # the resource, if the condition is true, the resource is deployed because there is exactly one iteration.
   count = (var.source_credentials_ssh_key != null && startswith(var.project_source_repo, "git@")) ? 1 : 0
 
@@ -32,7 +32,7 @@ resource "argocd_project" "this" {
   spec {
     description = "${var.name} application project"
 
-    # Concatenate the ApplicationSet repository with the allowed repositories in order to allow the ApplicationSet 
+    # Concatenate the ApplicationSet repository with the allowed repositories in order to allow the ApplicationSet
     # to be created in this project.
     source_repos = concat(
       [var.project_source_repo],
@@ -44,7 +44,7 @@ resource "argocd_project" "this" {
       namespace = var.project_dest_namespace
     }
 
-    # This destination block is needed in order to allow the ApplicationSet below to be created in the namespace 
+    # This destination block is needed in order to allow the ApplicationSet below to be created in the namespace
     # `argocd` while belonging to this project. This block is only needed if the user provides a namespace above
     # instead of the wildcard "*" configured by default.
     destination {
@@ -109,14 +109,19 @@ resource "argocd_application" "this" {
     }
 
     sync_policy {
-      automated = var.app_autosync
+      automated {
+        prune       = var.app_autosync.prune
+        self_heal   = var.app_autosync.self_heal
+        allow_empty = var.app_autosync.allow_empty
+      }
 
       retry {
-        backoff = {
-          duration     = ""
-          max_duration = ""
+        backoff {
+          duration     = "20s"
+          max_duration = "2m"
+          factor       = "2"
         }
-        limit = "0"
+        limit = "5"
       }
 
       sync_options = [

terraform.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     argocd = {
       source  = "oboukili/argocd"
-      version = ">= 4"
+      version = ">= 5"
     }
     utils = {
       source  = "cloudposse/utils"

commit 022d70da75c60f61dc18a0340e13b640fcfd2646

fix: readd support to deactivate auto-sync which was broken by #26

main.tf
@@ -39,7 +39,7 @@ resource "argocd_project" "this" {
       ["https://github.com/camptocamp/devops-stack-module-applicationset.git"]
     )
 
-    destination {
+    destination { # TODO Variabilize these values
       name      = "in-cluster"
       namespace = var.project_dest_namespace
     }
@@ -47,9 +47,9 @@ resource "argocd_project" "this" {
     # This destination block is needed in order to allow the ApplicationSet below to be created in the namespace
     # `argocd` while belonging to this project. This block is only needed if the user provides a namespace above
     # instead of the wildcard "*" configured by default.
-    destination {
+    destination { # TODO Variabilize these values
       name      = "in-cluster"
-      namespace = "argocd"
+      namespace = var.argocd_namespace
     }
 
     orphaned_resources {
@@ -103,16 +103,19 @@ resource "argocd_application" "this" {
       }
     }
 
-    destination {
+    destination { # TODO Variabilize these values
       name      = "in-cluster"
       namespace = var.argocd_namespace
     }
 
     sync_policy {
-      automated {
-        prune       = var.app_autosync.prune
-        self_heal   = var.app_autosync.self_heal
-        allow_empty = var.app_autosync.allow_empty
+      dynamic "automated" {
+        for_each = toset(var.app_autosync == { "allow_empty" = tobool(null), "prune" = tobool(null), "self_heal" = tobool(null) } ? [] : [var.app_autosync])
+        content {
+          prune       = automated.value.prune
+          self_heal   = automated.value.self_heal
+          allow_empty = automated.value.allow_empty
+        }
       }
 
       retry {